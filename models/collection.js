5d02494c72a1c30834cd87fe04d0b1adce8bae67
chenxiaofeihome <chenxiaofeihome@163.com>
2019-03-11 21:39:33 +0800

    完善目录结构

---
+++ models/collection.js
@@ -0,0 +1,10 @@
+const mongoose = require('../db/db.js');
+
+//生成数据模型，可以看到，我们下面创建了一个表，表中的数据有name、age、sex等字段，并且这些字段的数据类型也定义了，最后将model导出即可
+const collections = new mongoose.Schema({
+    userId :String,
+    songId : String,
+    addTime : String,
+});
+
+module.exports = mongoose.model('collections',collections);
